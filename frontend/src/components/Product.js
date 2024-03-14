0b6601a953d001da2d7af8e3fd6f39dcec563217
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-14 16:27:03 +0100

    7-Rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import products from '../products'
 import { Link } from 'react-router-dom'
+import Rating from './Rating';
 export const Product = ({Product}) => {
   return (
   <Card className='my-3 p-3 rounded'  >
@@ -12,10 +13,18 @@ export const Product = ({Product}) => {
         <Card.Body>
         <Link to={`/product/${Product._id}`}>
         <Card.Img src={Product.image} variant='top'/>
-        <Card.Title as="div">
+        <Card.Title as="div" className='product-title'>
         <strong>{Product.name}</strong>
         </Card.Title>
         </Link>
+
+
+        <Card.Text as='div'>
+        <Rating
+          value={Product.rating}
+          text={`${Product.numReviews} reviews`}
+        />
+      </Card.Text>
 
         <Card.Text as="h3">
         ${Product.price}
