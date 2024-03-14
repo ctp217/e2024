21fd2483abada9b247a686074e8d2eb8540dacc8
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-14 16:00:44 +0100

    Lesson 6-Router
    npm install --save react-router-dom@6.10.0
    
    npm install --save react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../assets/logo.png'
+import { LinkContainer } from "react-router-bootstrap";
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
-    <Navbar bg="dark" variant="dark" expand="lg"  collapseOnSelect>
-   <Container>
-   <Navbar.Brand href='/'>
-   <img src={logo} alt='CTP'/>
-   CTP</Navbar.Brand>
-   <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-<Navbar.Collapse id="basic-navbar-nav">
-<Nav className='ms-auto'>
-<Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
-<Nav.Link href='/cart'><FaUser/>Login</Nav.Link>
-</Nav>
-</Navbar.Collapse>
-   </Container>
-    </Navbar>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+        <LinkContainer to='/'>
+          <Navbar.Brand>
+            <img src={logo} alt="CTP" />
+            CTP
+          </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+            <LinkContainer to='/cart'>
+              <Nav.Link >
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+              <Nav.Link>
+                <FaUser />
+                Login
+              </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import products from '../products'
+import { Link } from 'react-router-dom'
 export const Product = ({Product}) => {
   return (
   <Card className='my-3 p-3 rounded'  >
-        <a href={`/product/${Product._id}`}>
+        <Link to={`/product/${Product._id}`}>
         <Card.Img src={Product.Img} variant='top'/>
-        </a>
+        </Link>
 
         <Card.Body>
-        <a href={`/product/${Product._id}`}>
+        <Link to={`/product/${Product._id}`}>
         <Card.Img src={Product.image} variant='top'/>
         <Card.Title as="div">
         <strong>{Product.name}</strong>
         </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="h3">
         ${Product.price}
