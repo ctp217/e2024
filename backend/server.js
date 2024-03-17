3fdfbcb2415594a424b06b20a16b9c64a02f912c
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-17 07:02:40 +0100

    12-get all Products and single product

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import products from'./data/products.js';
 
 const port =5000;
 
@@ -9,4 +10,14 @@ app.get('/',(req,res)=>{
     res.send('Nezar API is working... ')
 });
 
+//get all products
+app.get('/api/products',(req,res)=>{
+
+    res.json(products)
+});
+//get single product
+app.get('/api/products/:id',(req,res)=>{
+const product=products.find((p)=>p._id=== req.params.id);
+    res.json(product)
+});
 app.listen(port,()=>console.log(`Server is working on Port ${port}`));
