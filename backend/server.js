45ad301a546a9eff4bf94a6b581e072ce1688d5b
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-17 06:39:51 +0100

    11-server inial
    express

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
-console.log('==========hallo nizar=======');
-console.log();
-console.log('==========sever is working==============');
+import express from 'express';
+
+const port =5000;
+
+const app= express();
+
+app.get('/',(req,res)=>{
+
+    res.send('Nezar API is working... ')
+});
+
+app.listen(port,()=>console.log(`Server is working on Port ${port}`));
