82baf76a8caf186cefd295b7ca938c8e55f04d8d
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-18 06:20:47 +0100

    16-fetch data all products
    npm install --save-exact axios@^1.3.5

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,25 @@
 
+import { useEffect,useState } from 'react'
+
 import React from 'react'
 import { Row,Col } from 'react-bootstrap'
-import products from '../products'
+//import products from '../products'
 import { Product } from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+
+const [products,setProduct]=useState([]);
+
+useEffect(()=>{
+const fetchProducts=async() =>{
+  const {data}=await axios.get('/api/products/');
+  setProduct(data);
+};
+fetchProducts();
+},[]);
+
+
   return (
     <>
       <h1>Latest products</h1>
