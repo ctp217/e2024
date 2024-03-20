f43120a0aa98467a2f3628fd999058b3eba6ab30
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-20 13:47:49 +0100

    25-Router

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,19 @@
+import  express  from 'express';
+
+const router = express.Router();
+
+//import products from'./data/products.js';
+
+import products from '../data/products.js';
+//get all products
+router.get('/',(req,res)=>{
+
+  res.json(products)
+});
+//get single product
+router.get('/:id',(req,res)=>{
+const product=products.find((p)=>p._id === req.params.id);
+  res.json(product)
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js'
 
 
-import products from'./data/products.js';
 import connectDB from './config/db.js';
 dotenv.config();
 
@@ -18,14 +18,7 @@ app.get('/',(req,res)=>{
     res.send('Nezar API is working... ')
 });
 
-//get all products
-app.get('/api/products',(req,res)=>{
 
-    res.json(products)
-});
-//get single product
-app.get('/api/products/:id',(req,res)=>{
-const product=products.find((p)=>p._id === req.params.id);
-    res.json(product)
-});
+app.use('/api/products', productRoutes)
+
 app.listen(port,()=>console.log(`Server is working on Port ${port}`));
