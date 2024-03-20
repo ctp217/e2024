0e93c425254d32b1c46b13dd0c1e752e9b16166f
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-20 14:16:42 +0100

    26-Middleware and fetch data from data base using Model

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,14 +2,16 @@ import  express  from 'express';
 
 const router = express.Router();
 
-//import products from'./data/products.js';
+//import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js'
 
-import products from '../data/products.js';
+import Products from '../models/productModel.js'
 //get all products
-router.get('/',(req,res)=>{
-
+router.get('/',asyncHandler(async (req,res)=>{
+//to fetch data from data base 
+const products =await Products.find({});
   res.json(products)
-});
+}));
 //get single product
 router.get('/:id',(req,res)=>{
 const product=products.find((p)=>p._id === req.params.id);
