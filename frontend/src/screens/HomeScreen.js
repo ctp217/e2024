9a81a75690717ba9b16eccec94869d5dde36a44c
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-26 04:55:33 +0100

    ig

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,27 +9,22 @@ const HomeScreen = () => {
     <>
       {isLoading ? (
         <h2>..is Loading</h2>
-      ) : error ? (<div>{error.error} </div> ) : (
-        
+      ) : error ? (
+        <div>{error.error} </div>
+      ) : (
         <>
           <h1>Latest Products</h1>
           <Row>
-      {products.map((product)=>
-      (
-        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-      <Product Product={product} />
-        
-        </Col>
-      )
-        
-        )}
-      </Row>
-
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product Product={product} />
+              </Col>
+            ))}
+          </Row>
         </>
       )}
     </>
   );
-
 };
 
 export default HomeScreen;
