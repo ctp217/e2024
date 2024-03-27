992de34f49774aa35bfed7c53614dad82450e22c
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-27 22:05:01 +0100

    36-Add to Cart Function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,52 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+export const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      // NOTE: we don't need user, rating, numReviews or reviews
+      // in the cart
+
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate Item Price
+      const itemsPrice = (state.itemsPrice = addDecimals(
+        state.cartItems.reduce(
+          (acc, item) => acc + (item.price * 100 * item.qty) / 100,
+          0
+        )
+      ));
+
+      // Calculate the shipping price
+      //if itemprice>100 then shippingprice ==0 else 10
+      const shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // Calculate the tax price
+      const taxPrice = addDecimals(0.15 * state.itemsPrice);
+
+      const totalPrice = addDecimals(itemsPrice + shippingPrice + taxPrice);
+
+      // Save the cart to localStorage
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
