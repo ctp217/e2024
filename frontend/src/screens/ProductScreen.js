f1948fb310c7200cba36330dc017eb496d10f025
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-27 22:51:17 +0100

    37-show qty In stock

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import products from "../products";
 import React from "react";
@@ -11,6 +12,7 @@ import {
   Card,
   ListGroupItem,
   Button,
+  Form,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
@@ -18,6 +20,8 @@ import Message from "../components/Message";
 
 export const ProductScreen = () => {
   const { id: productId } = useParams();
+
+  const [qty, setQty] = useState(1);
 
   const {
     data: product,
@@ -33,11 +37,11 @@ export const ProductScreen = () => {
       <Link className="btn btn-primary my-3" to="/">
         Back
       </Link>
-    
+
       {isLoading ? (
-        <Loader/>
+        <Loader />
       ) : error ? (
-        <Message variant='danger'>{error.error} </Message>
+        <Message variant="danger">{error.error} </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
@@ -85,6 +89,29 @@ export const ProductScreen = () => {
                       </Col>
                     </Row>
                   </ListGroupItem>
+
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
 
                   <ListGroupItem>
                     <Button
