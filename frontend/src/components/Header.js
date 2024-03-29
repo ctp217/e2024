0091f3ad023ce3df0f7d3711d7b026bdc59bcc50
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-29 05:41:11 +0100

    40-show number items in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -22,6 +24,11 @@ const Header = () => {
               <Nav.Link >
                 <FaShoppingCart />
                 Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
               </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
