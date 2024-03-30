2dbf02391e3dd31e119518d3f3689e811eadfe28
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-03-30 11:04:57 +0100

    44-Create User Routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 
+import userRoutes from './routes/userRoutes.js';
+
 import connectDB from './config/db.js';
 dotenv.config();
 
@@ -21,6 +23,7 @@ app.get('/',(req,res)=>{
 
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
