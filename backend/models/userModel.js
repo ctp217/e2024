ef24bde4bcdf0baa6d5c1253b9b18aff372508c4
ctp217 <102032009+ctp217@users.noreply.github.com>
2024-04-02 06:56:08 +0200

    50-Register function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,11 +35,11 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // Encrypt password using bcrypt
+//pre : سيتم تنفيذ الوظيفة قبل حفظ البيانات في الداتا بيز
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
   }
-
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
